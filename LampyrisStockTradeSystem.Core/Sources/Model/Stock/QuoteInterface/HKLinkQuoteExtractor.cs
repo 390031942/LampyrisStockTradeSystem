f8e514b903aab8ceabd4fb2f803dbc5bb6fcbd2a
Administrator <Administrator@W501-2>
2024-03-27 19:44:32 +0800

    2024/3/27

--- LampyrisStockTradeSystem.Core/Sources/Model/Stock/QuoteInterface/HKLinkQuoteExtractor.cs
+++ LampyrisStockTradeSystem.Core/Sources/Model/Stock/QuoteInterface/HKLinkQuoteExtractor.cs
@@ -2,12 +2,24 @@
 
 public class HKLinkQuoteExtractor : IStockQuoteInterface
 {
-    public override StockQuoteInterfaceType quetoType => StockQuoteInterfaceType.CurrentQuotes;
+    public override StockQuoteInterfaceType quetoType => StockQuoteInterfaceType.HKLink;
 
-    protected override string url => "http://45.push2.eastmoney.com/api/qt/clist/get?cb=jQuery112403378116125837951_1711281402696&pn=1&pz=5000&po=1&np=1&ut=bd1d9ddb04089700cf9c27f6f7426281&fltt=2&invt=2&wbp2u=2710255628112086|0|1|0|web&fid=f3&fs=b:MK0146,b:MK0144&fields=f1,f2,f3,f4,f5,f6,f7,f8,f9,f10,f12,f13,f14,f15,f16,f17,f18,f19,f20,f21,f23,f24,f25,f26,f22,f33,f11,f62,f128,f136,f115,f152&_=1711281402699";
+    protected string url1 => "http://45.push2.eastmoney.com/api/qt/clist/get?cb=jQuery112403378116125837951_1711281402696&pn=1&pz=5000&po=1&np=1&ut=bd1d9ddb04089700cf9c27f6f7426281&fltt=2&invt=2&wbp2u=2710255628112086|0|1|0|web&fid=f3&fs=b:MK0146,b:MK0144&fields=f1,f2,f3,f4,f5,f6,f7,f8,f9,f10,f12,f13,f14,f15,f16,f17,f18,f19,f20,f21,f23,f24,f25,f26,f22,f33,f11,f62,f128,f136,f115,f152&_=1711281402699";
+    protected override string url => "http://45.push2.eastmoney.com/api/qt/clist/get";
     protected override Dictionary<string, string> parameters => new Dictionary<string, string>()
     {
-
+        { "cb", AppConfig.jQueryString },
+        { "po", "1" },
+        { "pz", "50000" },
+        { "pn", "1" },
+        { "np", "1" },
+        { "fltt", "2" },
+        { "invt", "2" },
+        { "ut", "b2884a393a59ad64002292a3e90d46a5" },
+        { "wbp2u","2710255628112086|0|1|0|web" },
+        { "fid","f3"},
+        { "fs","b:MK0146,b:MK0144"},
+        { "fields", "f1,f2,f3,f4,f5,f6,f7,f8,f9,f10,f12,f13,f14,f15,f16,f17,f18,f19,f20,f21,f23,f24,f25,f26,f22,f33,f11,f62,f128,f136,f115,f152" },
+        { "_","1711281402699"}
     };
-
 }
--- LampyrisStockTradeSystem.Core/Sources/UI/Custom/HKChaseRiseWindow.cs
+++ LampyrisStockTradeSystem.Core/Sources/UI/Custom/HKChaseRiseWindow.cs
@@ -5,18 +5,25 @@
 */
 
 using ImGuiNET;
+using Newtonsoft.Json.Linq;
 
 namespace LampyrisStockTradeSystem;
 
-public struct HKChaseRiseQuoteData
+public class HKChaseRiseQuoteData
 {
+    // 行情数据
     public QuoteData quoteData;
 
+    // K线数据
     public Bitmap klineImage;
     public int klineTextureId;
 
+    // 今日分时走势
     public Bitmap todayImage;
     public int todayImageTextureId;
+
+    // 日内 分钟-分时图 破新高次数
+    public int breakthroughTimes;
 }
 
 [UniqueWidget]
@@ -29,21 +36,82 @@ public class HKChaseRiseWindow:Widget
     }
 
     /// <summary>
+    /// 
+    /// </summary>
+    private static Dictionary<string,HKChaseRiseQuoteData> m_code2stockData = new Dictionary<string,HKChaseRiseQuoteData>();
+
+    /// <summary>
     /// 刷新港股通所有股票的行情
     /// </summary>
     [PlannedTask(executeTime ="09:29-16:10",executeMode = PlannedTaskExecuteMode.ExecuteDuringTime)]
     public static void RefreshHKStockQuote()
     {
-        HttpRequest.Get(StockQuoteInterface.Instance.GetQuoteUrl(StockQuoteInterfaceType.HKLink), (string json) => { 
-        
+        HttpRequest.Get(StockQuoteInterface.Instance.GetQuoteUrl(StockQuoteInterfaceType.HKLink), (string json) => {
+            string strippedJson = JsonStripperUtil.GetEastMoneyStrippedJson(json);
+            try
+            {
+                JObject jsonRoot = JObject.Parse(strippedJson);
+
+                JArray stockDataArray = jsonRoot?["data"]?["diff"]?.ToObject<JArray>();
+                if (stockDataArray != null)
+                {
+                    for (int i = 0; i < stockDataArray.Count; i++)
+                    {
+                        JObject stockObject = stockDataArray[i].ToObject<JObject>();
+
+                        if (stockObject != null)
+                        {
+                            // 这里获取股票代码和名称
+                            string name = stockObject["f14"]?.ToString();
+                            string code = stockObject["f12"]?.ToString();
+
+                            if (!string.IsNullOrEmpty(name) && !string.IsNullOrEmpty(code))
+                            {
+                                HKChaseRiseQuoteData stockData = null;
+                                if (!m_code2stockData.ContainsKey(code))
+                                {
+                                    stockData = m_code2stockData[code] = new HKChaseRiseQuoteData()
+                                    {
+                                        quoteData = new QuoteData(),
+                                    };
+                                }
+                                else
+                                {
+                                    stockData = m_code2stockData[code];
+                                }
+
+                                // 更新股票的实时行情
+                                // StockRealTimeQuoteData realTimeQuoteData = (StockRealTimeQuoteData)stockData.realTimeQuoteData;
+                                // realTimeQuoteData.kLineData.closePrice = stockObject["f2"].SafeToObject<float>(); // 现价
+                                // realTimeQuoteData.kLineData.percentage = stockObject["f3"].SafeToObject<float>(); // 涨幅
+                                // realTimeQuoteData.kLineData.priceChange = stockObject["f4"].SafeToObject<float>(); // 涨跌额
+                                // realTimeQuoteData.kLineData.volume = stockObject["f5"].SafeToObject<float>(); // 成交量
+                                // realTimeQuoteData.kLineData.money = stockObject["f6"].SafeToObject<float>(); // 成交额
+                                // realTimeQuoteData.kLineData.turnOverRate = stockObject["f8"].SafeToObject<float>(); // 换手率
+                                // realTimeQuoteData.kLineData.highestPrice = stockObject["f15"].SafeToObject<float>(); // 最高
+                                // realTimeQuoteData.kLineData.lowestPrice = stockObject["f16"].SafeToObject<float>(); // 最低
+                                // realTimeQuoteData.kLineData.openPrice = stockObject["f17"].SafeToObject<float>(); // 今开
+                                // realTimeQuoteData.riseSpped = stockObject["f22"].SafeToObject<float>(); // 涨速
+                                // realTimeQuoteData.bidAskData.theCommittee = stockObject["f31"].SafeToObject<float>(); // 买一价,可能是"-"
+                                // realTimeQuoteData.buyPrice = stockObject["f32"].SafeToObject<float>(); // 卖一价,可能是"-"
+                                // realTimeQuoteData.sellPrice = stockObject["f33"].SafeToObject<float>(); // 委比
+                                // realTimeQuoteData.transactionSumData.buyCount = stockObject["f34"].SafeToObject<float>(); // 外盘
+                                // realTimeQuoteData.transactionSumData.sellCount = stockObject["f35"].SafeToObject<float>(); // 内盘
+                            }
+                        }
+                    }
+
+                    LifecycleManager.Instance.Get<EventManager>().RaiseEvent(EventType.UpdateRealTimeQuotes);
+                }
+            }
+            catch (Exception ex)
+            {
+                // WidgetManagement.GetWidget<MessageBox>().SetContent("StockQuoteInterfaceType.CurrentQuotes报错", ex.ToString());
+            }
         });
     }
 
-    // 假设你已经有了一个窗口创建和ImGui渲染循环的基础设施
-    // 下面的方法应该在你的渲染循环中被调用
-
     private HttpClient httpClient = new HttpClient();
-    private static List<HKChaseRiseQuoteData> m_stockData = new List<HKChaseRiseQuoteData>();
 
     public override void OnGUI()
     {
@@ -55,7 +123,7 @@ public class HKChaseRiseWindow:Widget
             ImGui.TableSetupColumn("分时/K线图");
             ImGui.TableSetupColumn("操作按钮");
             ImGui.TableHeadersRow();
-
+            /*
             foreach (var item in m_stockData)
             {
                 ImGui.TableNextRow();
@@ -83,7 +151,7 @@ public class HKChaseRiseWindow:Widget
                     }
                 }
             }
-
+            */
             ImGui.EndTable();
         }
 
