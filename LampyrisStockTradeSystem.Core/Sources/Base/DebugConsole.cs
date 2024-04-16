501b1099fc54311fc7d4fee0c172ec82204f8132
wushuhong <390031942@qq.com>
2024-04-16 13:13:14 +0800

    2024/4/16

---
+++ LampyrisStockTradeSystem.Core/Sources/Base/DebugConsole.cs
@@ -0,0 +1,63 @@
+﻿namespace LampyrisStockTradeSystem;
+
+public enum DebugConsoleLogLevel
+{
+    [NamedValue("[普通]")]
+    Info = 1,
+
+    [NamedValue("[警告]")]
+    Warning = 2,
+
+    [NamedValue("[错误]")]
+    Error = 3
+}
+
+public class DebugConsoleLogData
+{
+    public DebugConsoleLogLevel level;
+    public string time;
+    public string message;
+
+    private string m_string;
+
+    public override string ToString()
+    {
+        if(m_string == null)
+        {
+            m_string ="[" + time + "]: " + EnumNameManager.GetName(level) + " " +message; 
+        }
+        return m_string;
+    }
+}
+
+public class DebugConsole:Singleton<DebugConsole>
+{
+    private List<DebugConsoleLogData> m_debugConsoleLogDataList = new List<DebugConsoleLogData>();
+
+    public List<DebugConsoleLogData> debugConsoleLogDatas => m_debugConsoleLogDataList;
+
+    public void Log(string message, DebugConsoleLogLevel level = DebugConsoleLogLevel.Info)
+    {
+        m_debugConsoleLogDataList.Add(new DebugConsoleLogData()
+        {
+            message = message,
+            level = level,
+            time = DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss SSS")
+        });
+    }
+
+    public void LogException(Exception ex)
+    {
+        m_debugConsoleLogDataList.Add(new DebugConsoleLogData()
+        {
+            message = ex.ToString(),
+            level = DebugConsoleLogLevel.Error,
+            time = DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss SSS")
+        });
+    }
+
+    public void Clear()
+    {
+        m_debugConsoleLogDataList.Clear();
+    }
+}
---
+++ LampyrisStockTradeSystem.Core/Sources/UI/Core/DebugLogConsoleWindow.cs
@@ -0,0 +1,34 @@
+﻿using ImGuiNET;
+
+namespace LampyrisStockTradeSystem;
+
+[UniqueWidget]
+public class DebugLogConsoleWindow : Widget
+{
+    private List<string> logs = new List<string>();
+
+    private string searchText = string.Empty;
+
+    public override void OnGUI()
+    {
+        // 搜索框
+        ImGui.InputText("##SearchText", ref searchText, 255);
+
+        // 清除按钮
+        if (ImGui.Button("清除"))
+        {
+            DebugConsole.Instance.Clear();
+        }
+
+        // 滚动区域
+        ImGui.BeginChild("滚动区域");
+        foreach (var log in logs)
+        {
+            if (string.IsNullOrEmpty(searchText) || log.Contains(searchText, StringComparison.OrdinalIgnoreCase))
+            {
+                ImGui.TextUnformatted(log);
+            }
+        }
+        ImGui.EndChild();
+    }
+}
